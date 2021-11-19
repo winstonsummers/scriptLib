02dcd81c22ad7d2bca88ea66199a71c659c5131e
winstonsummers <33791219+winstonsummers@users.noreply.github.com>
2021-11-19 14:03:59 -0800

    testing commit message

--- git_helpers.zsh
+++ git_helpers.zsh
@@ -3,25 +3,32 @@ alias gcm="(git checkout develop || git checkout main || git checkout master) &&
 
 # add and commit changes quickly
 function _gac_helper() {
-    git add -A
-
     p_flag=""
     while getopts "p" flag; do
         case "${flag}" in
             p) p_flag="true" 
                 shift ;;
-            *) exit 1 ;;
+            *) echo "Invalid flag:"
+                echo " -p is the only valid option."
+                exit 1 ;;
         esac
     done
 
-    git commit -m "$*"
+    git add -A
+
+    commitMessage="$*"
+    if [[ "${commitMessage}" == "" ]]
+        then
+            vared -p "Commit Message: " commitMessage
+    fi
+
+    git commit -m "${commitMessage}"
 
     if [[ "${p_flag}" == "true" ]]
         then
             git push
     fi
 }
-# need changes for test purposes
 
 alias gac=_gac_helper
-alias gacp="gac -p \"$*\""
+alias gacp=_gac_helper -p "$*"
