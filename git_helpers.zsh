7e2eb346d6d62e8425718854821203479ed42e0f
winstonsummers <33791219+winstonsummers@users.noreply.github.com>
2021-11-19 14:56:20 -0800

    check for remote first

--- git_helpers.zsh
+++ git_helpers.zsh
@@ -3,6 +3,7 @@ alias gcm="(git checkout develop || git checkout main || git checkout master) &&
 
 # add and commit changes quickly
 function _gac_helper() {
+    # validate flags
     p_flag=""
     while getopts "p" flag; do
         case "${flag}" in
@@ -10,26 +11,39 @@ function _gac_helper() {
                 shift ;;
             *) echo "Invalid flag:"
                 echo " -p is the only valid option."
-                exit 1 ;;
+                return
         esac
     done
 
-    git add -A
-
+    # validate commit message
     commitMessage="$*"
     if [[ "${commitMessage}" == "" ]]
         then
             vared -p "Commit Message: " commitMessage
     fi
 
+    # Do The Things!!!
+    echo "adding all the things"
+    git add -A
+
+    echo "committing all the changes"
     git commit -m "${commitMessage}"
 
     if [[ "${p_flag}" == "true" ]]
         then
-            git push
+            if [[ $(git config --get remote.origin.url) ]]
+                then
+                    echo "pushing all the commits"
+                    git push
+                else
+                    echo "no remote found..."
+                    echo "skipping push"
+            fi
     fi
+
+    echo "le fine"
 }
 
 
 alias gac=_gac_helper
-alias gacp=_gac_helper -p "$*"
+alias gacp=_gac_helper -p
